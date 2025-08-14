141a9a9f9541d374dbb3d501e950b2ee7e0760c8
higcclg-web <higcclg@gmail.com>
2025-08-14 12:33:34 -0400

    Update db.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,13 +1,14 @@
-// src/lib/db.ts
 import { PrismaClient } from "@prisma/client";
 
 declare global {
   // eslint-disable-next-line no-var
-  var prismaGlobal: PrismaClient | undefined;
+  var prisma: PrismaClient | undefined;
 }
 
-export const prisma = globalThis.prismaGlobal ?? new PrismaClient();
+export const prisma =
+  global.prisma ??
+  new PrismaClient({
+    log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+  });
 
-if (process.env.NODE_ENV !== "production") {
-  globalThis.prismaGlobal = prisma;
-}
+if (process.env.NODE_ENV !== "production") global.prisma = prisma;
